30c4e3af1ca570db54308189c57e5dd4047ef78d
Jacques Tea <jacquestea16@gmail.com>
2025-06-07 12:03:24 +0200

    Add sound effects

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -5,6 +5,7 @@ extends RigidBody2D
 @export var bounce_acceleration_multiplier: float = 1.05
 
 @onready var screen_size: Vector2 = get_viewport_rect().size
+@onready var bounce_sound: AudioStreamPlayer2D = $BounceSound
 
 const DEFAULT_SPEED: float = 300.0
 
@@ -14,6 +15,7 @@ func _process(delta: float) -> void:
 	var collision_data := move_and_collide(velocity * speed * delta)
 	if collision_data:
 		velocity = velocity.bounce(collision_data.get_normal())
+		play_bounce_sound()
 
 func launch() -> void:
 	velocity = Vector2(randf_range(-1.0, 1.0), 0.0).normalized()
@@ -23,8 +25,13 @@ func bounce_on_paddle(paddle_pos_y: float, paddle_height: float) -> void:
 	var new_move_dir: float = (global_position.y - paddle_pos_y) / (paddle_height / 2)
 	velocity = Vector2(-velocity.x, new_move_dir)
 	speed *= bounce_acceleration_multiplier
+	play_bounce_sound()
 	
 func reset() -> void:
 	global_position = screen_size / 2
 	speed = DEFAULT_SPEED
 	velocity = Vector2.ZERO
+
+func play_bounce_sound() -> void:
+	bounce_sound.pitch_scale = randf_range(0.5, 1.5)
+	bounce_sound.play()
--- scripts/score_detector.gd
+++ scripts/score_detector.gd
@@ -4,6 +4,9 @@ signal player_scored(is_right: bool)
 
 @export var is_right: bool = false
 
+@onready var score_sound: AudioStreamPlayer2D = $ScoreSound
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Ball:
 		player_scored.emit(is_right)
+		score_sound.play()
